b29d2cda0b80c79c31f77660796b3fac78a86b9c
Lars Mäder <mail@larsmaeder.ch>
2022-12-13 16:16:37 +0100

    ADD: tooltip, copy function

--- src/components/copyURL/copyURL.jsx
+++ src/components/copyURL/copyURL.jsx
@@ -1,14 +1,37 @@
 import * as React from "react";
-import { IconButton } from "@chakra-ui/react";
+import { useState } from "react";
+import { IconButton, Tooltip } from "@chakra-ui/react";
 import { CopyIcon } from "@chakra-ui/icons";
 
 const CopyURL = () => {
+  const currentUrl = window.location.href;
+  const [copied, setCopied] = useState(false);
+  function CopyLink() {
+    // using the Clipboard API to copy the current page's link to the clipboard
+    navigator.clipboard.writeText(currentUrl);
+    setCopied(true);
+  }
   return (
-    <IconButton
-      aria-label="Search database"
-      icon={<CopyIcon />}
-      variant="sundial"
-    />
+    <>
+      <Tooltip
+        aria-label="Click to copy page link"
+        label="Copy page link"
+        placement="bottom-start"
+      >
+        <IconButton
+          aria-label="Search database"
+          icon={<CopyIcon />}
+          color="sundial.500"
+          background="sundial.100"
+          _hover={{ background: "sundial.200 " }}
+          size="lg"
+          onClick={CopyLink}
+        />
+      </Tooltip>
+      {/* <div>
+        {copied ? "Link copied to clipboard!" : "Copy link to clipboard"}
+      </div> */}
+    </>
   );
 };
 
