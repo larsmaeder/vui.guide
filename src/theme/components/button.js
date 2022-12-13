825f31b7df96940f0110067367ed565a65a5fda0
Lars Mäder <mail@larsmaeder.ch>
2022-12-13 16:15:49 +0100

    THEME: button.js

--- src/theme/components/button.js
+++ src/theme/components/button.js
@@ -2,19 +2,13 @@ import { defineStyleConfig } from "@chakra-ui/react";
 
 export const ButtonStyles = defineStyleConfig({
   // Styles for the base style
-  baseStyle: {},
+  baseStyle: {
+    borderRadius: "xl",
+  },
   // Styles for the size variations
   sizes: {},
   // Styles for the visual style variations
-  variants: {
-    sundial: {
-      borderRadius: "full",
-      background: "sundial.100",
-      height: "40px",
-      width: "40px",
-      color: "yellow.400",
-    },
-  },
+  variants: {},
   // The default `size` or `variant` values
   defaultProps: {},
 });
