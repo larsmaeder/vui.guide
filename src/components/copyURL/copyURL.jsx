7497268f3b9714f71c9adf771f54b19ad2d0c7a7
Lars Mäder <mail@larsmaeder.ch>
2022-12-14 17:15:32 +0100

    MOD: components

--- src/components/copyURL/copyURL.jsx
+++ src/components/copyURL/copyURL.jsx
@@ -1,15 +1,23 @@
 import * as React from "react";
-import { useState } from "react";
-import { IconButton, Tooltip } from "@chakra-ui/react";
+// import { useState } from "react";
+import { IconButton, Tooltip, useToast } from "@chakra-ui/react";
 import { CopyIcon } from "@chakra-ui/icons";
 
 const CopyURL = () => {
   const currentUrl = window.location.href;
-  const [copied, setCopied] = useState(false);
+  const toast = useToast();
   function CopyLink() {
     // using the Clipboard API to copy the current page's link to the clipboard
     navigator.clipboard.writeText(currentUrl);
-    setCopied(true);
+    // toast feedback
+    toast({
+      title: "Ready to share!",
+      description: "We’ve copied the page link to your clipboard.",
+      duration: 3000,
+      isClosable: true,
+      position: "top",
+      status: "success",
+    });
   }
   return (
     <>
@@ -28,9 +36,6 @@ const CopyURL = () => {
           onClick={CopyLink}
         />
       </Tooltip>
-      {/* <div>
-        {copied ? "Link copied to clipboard!" : "Copy link to clipboard"}
-      </div> */}
     </>
   );
 };
