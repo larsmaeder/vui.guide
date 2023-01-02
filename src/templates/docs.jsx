f98d3ccc968f7218a8aa6f4f293699ba47421290
larsmaeder <mail@larsmaeder.ch>
2023-01-03 00:12:12 +0100

    refactor: template

--- src/templates/docs.jsx
+++ src/templates/docs.jsx
@@ -2,22 +2,30 @@ import * as React from "react";
 import { graphql, Link } from "gatsby";
 import { MDXProvider } from "@mdx-js/react";
 import theme from "../theme";
-import { Prose } from "@nikolovlazar/chakra-ui-prose";
 import {
   ChakraProvider,
+  Grid,
+  GridItem,
+  Flex,
+  Box,
   Stack,
   Heading,
-  Stat,
-  StatLabel,
-  StatNumber,
+  Text,
+  IconButton,
+  Tooltip,
+  Avatar,
+  AvatarGroup,
 } from "@chakra-ui/react";
+import { SkipNavLink, SkipNavContent } from "@chakra-ui/skip-nav";
+import { MdHome } from "react-icons/md";
 import Logo from "../components/Logo";
 import CopyURL from "../components/CopyURL";
 import CustomBreadcrumb from "../components/CustomBreadcrumb";
-
-const shortcodes = { Link };
+import TimeToRead from "../components/TimeToRead";
+import SideNavigation from "../components/SideNavigation";
 
 const DocsTemplate = ({ data, children, location, pageContext }) => {
+  const shortcodes = { Link };
   const currentUrl = data.site.siteMetadata.siteUrl + location.pathname;
   const {
     breadcrumb: { crumbs },
@@ -25,32 +33,107 @@ const DocsTemplate = ({ data, children, location, pageContext }) => {
   const CustomBreadcrumbHidden = () => {
     if (!(location.pathname === "/docs/")) {
       return (
-        <CustomBreadcrumb
-          crumbs={crumbs}
-          crumbLabel={data.mdx.frontmatter.title}
-          crumbLocationRef={location.pathname}
-          hiddenCrumbs={["/"]}
-        />
+        <Flex align="stretch">
+          <CopyURL url={currentUrl} />
+          <CustomBreadcrumb
+            crumbs={crumbs}
+            crumbLabel={data.mdx.frontmatter.title}
+            crumbLocationRef={location.pathname}
+            hiddenCrumbs={["/"]}
+          />
+        </Flex>
       );
+    } else {
+      return <CopyURL url={currentUrl} standalone />;
     }
+  };
+  const layoutWidth = {
+    base: "full",
+    lg: "60em",
+    xl: "70em",
+    "2xl": "90em",
+  };
+  const layoutBorder = {
+    base: "4",
+    md: "8",
+    lg: "12",
   };
   return (
     <ChakraProvider theme={theme}>
-      <Logo />
-      <Stack spacing={3} direction="row" alignItems="center">
-        <CopyURL currentUrl={currentUrl} />
-        <CustomBreadcrumbHidden />
-      </Stack>
-      <Heading as="h1" size="3xl">
-        {data.mdx.frontmatter.title}
-      </Heading>
-      <Stat>
-        <StatLabel>Time to read</StatLabel>
-        <StatNumber>{data.mdx.fields.timeToRead.text}</StatNumber>
-      </Stat>
-      <Prose>
-        <MDXProvider components={shortcodes}>{children}</MDXProvider>
-      </Prose>
+      <SkipNavLink>Skip to content</SkipNavLink>
+      <Flex justify="center" paddingX={layoutBorder}>
+        <Box w={layoutWidth}>
+          <Grid templateColumns="repeat(3, 1fr)" gap={12}>
+            <GridItem
+              colSpan={1}
+              // borderRight="1px"
+              // borderColor="gray.100"
+              display={{ base: "none", lg: "block" }}
+            >
+              <Box pos="sticky" top={0} pt={12}>
+                <Logo />
+                <Box as="nav" pt={32}>
+                  <SideNavigation data={data.allMdx.edges} />
+                </Box>
+              </Box>
+            </GridItem>
+            <GridItem colSpan={2}>
+              <Box as="main" pt={32}>
+                <Stack spacing={3} direction="row" align="center">
+                  <Tooltip
+                    aria-label="Click to go home"
+                    label="Home"
+                    placement="top-start"
+                  >
+                    <IconButton
+                      aria-label="Go home"
+                      icon={<MdHome />}
+                      color="sundial.500"
+                      background="sundial.100"
+                      _hover={{ background: "sundial.200 " }}
+                      _active={{ background: "sundial.300 " }}
+                      size="md"
+                      as={Link}
+                      to="/"
+                    />
+                  </Tooltip>
+                  <CustomBreadcrumbHidden />
+                </Stack>
+                <SkipNavContent />
+                <Box maxW="40ch" pt={16}>
+                  <Heading as="h1" color="purple.600" fontSize="5xl">
+                    {data.mdx.frontmatter.title}
+                  </Heading>
+                  <Text fontFamily="heading" pt={6} fontSize="sm">
+                    {data.mdx.frontmatter.prelude}
+                  </Text>
+                </Box>
+                <Box>
+                  <Text>Contributors:</Text>
+                  <AvatarGroup
+                    size="sm"
+                    max={2}
+                    fontFamily="heading"
+                    fontSize="sm"
+                  >
+                    <Avatar name="Lars Mäder" />
+                    <Avatar name="Segun Adebayo" />
+                    <Avatar name="Kent Dodds" />
+                    <Avatar name="Prosper Otemuyiwa" />
+                    <Avatar name="Christian Nwamba" />
+                  </AvatarGroup>
+                </Box>
+                <TimeToRead time={data.mdx.fields.timeToRead.text} />
+                <MDXProvider components={shortcodes}>
+                  <Box maxW="42ch" className="mdx-prose">
+                    {children}
+                  </Box>
+                </MDXProvider>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Box>
+      </Flex>
     </ChakraProvider>
   );
 };
@@ -77,6 +160,20 @@ export const pageQuery = graphql`
         title
         navDocTitle
         toc
+        prelude
+      }
+    }
+    allMdx {
+      edges {
+        node {
+          frontmatter {
+            category
+            slug
+            title
+            navDocTitle
+            sort
+          }
+        }
       }
     }
   }
