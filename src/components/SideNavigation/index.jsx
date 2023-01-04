08885b20361e33c5fc60c79c6f04d696c01f5b05
Lars Mäder <mail@larsmaeder.ch>
2023-01-04 17:07:24 +0100

    refactor: component

--- src/components/SideNavigation/index.jsx
+++ src/components/SideNavigation/index.jsx
@@ -7,6 +7,7 @@ import {
   AccordionPanel,
   Text,
   HStack,
+  Flex,
   Icon,
   Link,
 } from "@chakra-ui/react";
@@ -15,20 +16,52 @@ import {
   MdKeyboardArrowRight,
   MdLibraryBooks,
 } from "react-icons/md";
-import { Link as GatsbyLink } from "gatsby";
+import { Link as GatsbyLink, useStaticQuery, graphql } from "gatsby";
 import { v4 as uuidv4 } from "uuid";
 
-const SideNavigation = ({ data: node }) => {
+const SideNavigation = ({
+  data: node,
+  crumbs: autoGenCrumbs,
+  crumbLocationRef,
+}) => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      mdx {
+        frontmatter {
+          category
+        }
+      }
+    }
+  `);
+  console.log(data.mdx.frontmatter.category);
+
+  // let isCurrentPage;
   const categories = [
     ...new Map(node.map((c) => [c.node.frontmatter.category, c])).values(),
   ];
+  // autoGenCrumbs.map((c) => {
+  //   console.log(autoGenCrumbs)
+  //   isCurrentPage = crumbLocationRef === c.pathname;
+  //   return console.log(isCurrentPage ? c.crumbLabel : null);
+  // });
+  // const test = () => {
+  //   let finalnumber;
+
+  //   categories.findIndex((x) => x.node.frontmatter.category === "Fundamentals");
+  //   return finalnumber;
+  // };
+  const currentItem = Array.of(
+    categories.findIndex((x) => x.node.frontmatter.category === "Fundamentals")
+  );
+  console.log(currentItem);
+
   return (
     <>
       <HStack spacing={2} paddingLeft={3}>
         <Icon as={MdLibraryBooks} />
         <Text fontFamily="heading">Guidelines</Text>
       </HStack>
-      <Accordion allowToggle>
+      <Accordion defaultIndex={currentItem} allowMultiple>
         {categories.map((c, i) => {
           const category = c.node.frontmatter.category;
           if (category === null) {
@@ -44,26 +77,28 @@ const SideNavigation = ({ data: node }) => {
                       ) : (
                         <Icon as={MdKeyboardArrowRight} />
                       )}
-                      <Box as="span" flex="1" textAlign="left">
-                        {category}
-                      </Box>
+                      <Box as="span">{category}</Box>
                     </AccordionButton>
                     <AccordionPanel pb={4}>
-                      {node.map((c, i) => {
-                        if (category === c.node.frontmatter.category) {
-                          return (
-                            <Link
-                              as={GatsbyLink}
-                              to={c.node.frontmatter.slug}
-                              key={i + uuidv4}
-                              display="block"
-                              py={3}
-                            >
-                              {c.node.frontmatter.navDocTitle}
-                            </Link>
-                          );
-                        } else return null;
-                      })}
+                      <Flex direction="column" alignItems="self-start">
+                        {node.map((c, i) => {
+                          if (category === c.node.frontmatter.category) {
+                            return (
+                              <Link
+                                as={GatsbyLink}
+                                to={c.node.frontmatter.slug}
+                                key={i + uuidv4}
+                                borderLeft="1px solid red"
+                                px={6}
+                                py={3}
+                                _hover={{ borderLeft: "1px solid blue" }}
+                              >
+                                {c.node.frontmatter.navDocTitle}
+                              </Link>
+                            );
+                          } else return null;
+                        })}
+                      </Flex>
                     </AccordionPanel>
                   </>
                 )}
