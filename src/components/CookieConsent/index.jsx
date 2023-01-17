673dbb9e7f5922fcca76ed3523ba9b9fa20846e5
Lars Mäder <mail@larsmaeder.ch>
2023-01-17 16:58:38 +0100

    refactor: cookie consent

--- src/components/CookieConsent/index.jsx
+++ src/components/CookieConsent/index.jsx
@@ -1,44 +1,71 @@
 import * as React from "react";
-import { instanceOf } from "prop-types";
 import { motion } from "framer-motion";
-import { Button, ButtonGroup, Box } from "@chakra-ui/react";
+import { chakra, Button, ButtonGroup, Box, Text } from "@chakra-ui/react";
 import Typewriter from "typewriter-effect";
-import { useCookies, withCookies, Cookies } from "react-cookie";
 
-const Typer = () => {
+const AskForCookie = ({ setCookie, removeCookie }) => {
+  const handleCookieAccept = () => {
+    setCookie("consent", "accepted", { path: "/" });
+    console.log("Accepted");
+  };
+  const handleCookieDecline = () => {
+    setCookie("consent", "declined", { path: "/" });
+    console.log("Declined");
+    removeCookie("_ga");
+  };
   const [isComplete, setIsComplete] = React.useState(false);
   const init = (typewriter) => {
     typewriter
+      .pauseFor(1500)
+      .typeString("Hi, I am Vox.")
+      .pauseFor(1000)
+      .pasteString("<br>")
+      .typeString("It seems like we don’t know each other yet.")
       .pauseFor(500)
-      .typeString("Hi im Vox. Seems like it's your first time here.")
-      .start()
-      .callFunction(() => setIsComplete(!isComplete));
+      .typeString(" I like cookies, do you?")
+      .callFunction(() => setIsComplete(!isComplete))
+      .start();
   };
   return (
     <>
-      <Typewriter
-        onInit={init}
-        options={{
-          loop: false,
-          delay: 1,
+      <chakra.div
+        color="font.100"
+        fontSize={{
+          base: "4xl",
+          sm: "5xl",
+          md: "6xl",
+          xl: "8xl",
         }}
-      />
+      >
+        <Typewriter
+          onInit={init}
+          options={{
+            loop: false,
+            delay: 30,
+          }}
+        />
+      </chakra.div>
       {isComplete ? (
-        <>
-        {console.log("completed")}
-        <motion.div
-          initial={{ opacity: 0, y: "0px" }}
-          whileInView={{ opacity: 1, y: "-5px" }}
-          transition={{ duration: 0.4 }}
-          viewport={{ once: true }}
-        >
-          <Box>Do you want Cookies?</Box>
-          <ButtonGroup pt={16} colorScheme="purple" size="lg">
-            <Button>Accept Cookie</Button>
-            <Button>Decline Cookie</Button>
-          </ButtonGroup>
-        </motion.div>
-        </>
+        <Box mt={12}>
+          {console.log("completed")}
+          <motion.div
+            initial={{ opacity: 0, y: "0px" }}
+            whileInView={{ opacity: 1, y: "-5px" }}
+            transition={{ duration: 0.4, delay: 0.2 }}
+            viewport={{ once: true }}
+          >
+            <Text fontSize="xl">
+              I collect cookies to analyze website traffic and performance. I
+              never collect personal information from you.
+            </Text>
+            <ButtonGroup pt={6} colorScheme="purple" size="lg">
+              <Button onClick={handleCookieAccept}>Accept Cookies</Button>
+              <Button variant="ghost" onClick={handleCookieDecline}>
+                Decline Cookies
+              </Button>
+            </ButtonGroup>
+          </motion.div>
+        </Box>
       ) : (
         console.log("not completed")
       )}
@@ -46,68 +73,60 @@ const Typer = () => {
   );
 };
 
-const CookieConsent = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(["consent"]);
-  setCookie("consent", "decline", { path: "/" });
-  const handleCookieAccept = () => {
-    setCookie("consent", "accept", { path: "/" });
-    console.log("Accepted");
-  };
-  const handleCookieDecline = () => {
-    setCookie("consent", "decline", { path: "/" });
-    console.log("Declined");
-    removeCookie("_ga");
+const Introduction = () => {
+  const [isComplete, setIsComplete] = React.useState(false);
+  const init = (typewriter) => {
+    typewriter
+      .pauseFor(1000)
+      .typeString("What would you like to do?")
+      .start()
+      .callFunction(() => setIsComplete(!isComplete));
   };
   return (
     <>
-      <Typer />
+      <chakra.div
+        color="font.100"
+        fontSize={{
+          base: "4xl",
+          sm: "5xl",
+          md: "6xl",
+          xl: "8xl",
+        }}
+      >
+        <Typewriter
+          onInit={init}
+          options={{
+            loop: false,
+            delay: 16,
+          }}
+        />
+      </chakra.div>
+      {isComplete ? (
+        <Box>
+          {console.log("completed")}
+          <motion.div
+            initial={{ opacity: 0, y: "0px" }}
+            whileInView={{ opacity: 1, y: "-5px" }}
+            transition={{ duration: 0.4, delay: 0.2 }}
+            viewport={{ once: true }}
+          >
+            <ButtonGroup pt={16} spacing={6} colorScheme="purple" size="lg">
+              <Button>Read Guide</Button>
+              <Button variant="link">Learn more about</Button>
+            </ButtonGroup>
+          </motion.div>
+        </Box>
+      ) : (
+        console.log("not completed")
+      )}
     </>
   );
 };
 
-/* {isComplete && (
-          <motion.div
-            initial={{ opacity: 0, y: "0px" }}
-            whileInView={{ opacity: 1, y: "-5px" }}
-            transition={{ duration: 0.4 }}
-            viewport={{ once: true }}
-          >
-            <ButtonGroup pt={16} colorScheme="purple" size="lg">
-              <Button>Read Guide</Button>
-              <Button variant="ghost">Learn more</Button>
-            </ButtonGroup>
-          </motion.div>
-        )} */
-//   else
-//     return (
-//       <>
-//         <Typewriter
-//           onInit={init}
-//           options={{
-//             loop: false,
-//           }}
-//         />
-//         {/* {isComplete && (
-// <motion.div
-//   initial={{ opacity: 0, y: "0px" }}
-//   whileInView={{ opacity: 1, y: "-5px" }}
-//   transition={{ duration: 0.4 }}
-//   viewport={{ once: true }}
-// >
-//   <Box>Do you want Cookies?</Box>
-//   <ButtonGroup pt={16} colorScheme="purple" size="lg">
-//     <Button onClick={() => handleCookieAccept}>Accept Cookie</Button>
-//     <Button onClick={() => handleCookieDecline}>
-//       Decline Cookie
-//     </Button>
-//   </ButtonGroup>
-// </motion.div>
-//         )} */}
-//       </>
-//     );
-
-CookieConsent.propTypes = {
-  cookies: instanceOf(Cookies).isRequired,
+const CookieConsent = ({ cookies, setCookie, removeCookie }) => {
+  console.log(cookies.consent);
+  if (cookies.consent === "accepted") return <Introduction />;
+  else return <AskForCookie {...{ setCookie, removeCookie }} />;
 };
 
-export default withCookies(CookieConsent);
+export default CookieConsent;
