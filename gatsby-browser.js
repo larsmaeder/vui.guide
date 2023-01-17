8a906fc3fb33cad34fb9ba6e446f5eee0d1e5ad4
Lars Mäder <mail@larsmaeder.ch>
2023-01-17 16:53:50 +0100

    feat: custom context provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,12 @@
+// import * as React from "react";
 import "@fontsource/readex-pro/400.css"; // Normal
 import "@fontsource/readex-pro/700.css"; // Bold
 import "@fontsource/kaisei-decol/400.css"; // Normal
 import "@fontsource/kaisei-decol/700.css"; // Bold
 import "@fontsource/roboto-mono/400.css"; // Normal
 import "@fontsource/roboto-mono/700.css"; // Bold
+// import { VuiGuideProvider } from "./src/context/VuiGuideContext"
+
+// export const wrapRootElement = ({ element }) => (
+//   <VuiGuideProvider>{element}</VuiGuideProvider>
+// )
