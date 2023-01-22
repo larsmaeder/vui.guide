d8eb0509d8b255da78641f3c1656c2a7a28bcf4f
larsmaeder <mail@larsmaeder.ch>
2023-01-22 23:29:14 +0100

    refactor: cookie consent

--- src/components/CookieConsent/index.jsx
+++ src/components/CookieConsent/index.jsx
@@ -1,7 +1,8 @@
 import * as React from "react";
+import { Link as GatsbyLink } from "gatsby";
 import { motion } from "framer-motion";
-import { chakra, Button, ButtonGroup, Box, Text } from "@chakra-ui/react";
-import Typewriter from "typewriter-effect";
+import { Button, ButtonGroup, Box, Text } from "@chakra-ui/react";
+import ChatBot from "react-simple-chatbot";
 
 const AskForCookie = ({ setCookie, removeCookie }) => {
   const handleCookieAccept = () => {
@@ -13,120 +14,120 @@ const AskForCookie = ({ setCookie, removeCookie }) => {
     console.log("Declined");
     removeCookie("_ga");
   };
-  const [isComplete, setIsComplete] = React.useState(false);
-  const init = (typewriter) => {
-    typewriter
-      .pauseFor(1500)
-      .typeString("Hi, I am Vox.")
-      .pauseFor(1000)
-      .pasteString("<br>")
-      .typeString("It seems like we don’t know each other yet.")
-      .pauseFor(500)
-      .typeString(" I like cookies, do you?")
-      .callFunction(() => setIsComplete(!isComplete))
-      .start();
-  };
   return (
-    <>
-      <chakra.div
-        color="font.100"
-        fontSize={{
-          base: "4xl",
-          sm: "5xl",
-          md: "6xl",
-          xl: "8xl",
-        }}
-      >
-        <Typewriter
-          onInit={init}
-          options={{
-            loop: false,
-            delay: 30,
-          }}
-        />
-      </chakra.div>
-      {isComplete ? (
-        <Box mt={12}>
-          {console.log("completed")}
-          <motion.div
-            initial={{ opacity: 0, y: "0px" }}
-            whileInView={{ opacity: 1, y: "-5px" }}
-            transition={{ duration: 0.4, delay: 0.2 }}
-            viewport={{ once: true }}
-          >
-            <Text fontSize="xl">
-              I collect cookies to analyze website traffic and performance. I
-              never collect personal information from you.
-            </Text>
-            <ButtonGroup pt={6} colorScheme="purple" size="lg">
-              <Button onClick={handleCookieAccept}>Accept Cookies</Button>
-              <Button variant="ghost" onClick={handleCookieDecline}>
-                Decline Cookies
-              </Button>
-            </ButtonGroup>
-          </motion.div>
-        </Box>
-      ) : (
-        console.log("not completed")
-      )}
-    </>
+    <ChatBot
+      hideHeader={true}
+      width="100%"
+      height="auto"
+      hideBotAvatar={true}
+      hideUserAvatar={true}
+      className="default"
+      steps={[
+        {
+          id: "1",
+          message: "Hi, I’m Vox.",
+          trigger: "2",
+          hideInput: true,
+        },
+        {
+          id: "2",
+          message: "It seems like we don’t know each other yet.",
+          trigger: "3",
+          hideInput: true,
+        },
+        {
+          id: "3",
+          message:
+            "I’ll start with a fact about me: I need cookies to function properly.",
+          trigger: "4",
+          hideInput: true,
+        },
+        {
+          id: "4",
+          message: "Do you want to accept cookies?",
+          trigger: "5",
+          hideInput: true,
+        },
+        {
+          id: "5",
+          component: (
+            <Box mt={12}>
+              <motion.div
+                initial={{ opacity: 0, y: "0px" }}
+                whileInView={{ opacity: 1, y: "-5px" }}
+                transition={{ duration: 0.4, delay: 0.2 }}
+                viewport={{ once: true }}
+              >
+                <Text fontSize="xl">
+                  I use cookies to analyze website traffic and performance. I
+                  never collect personal information from you.
+                </Text>
+                <ButtonGroup pt={6} colorScheme="purple" size="lg">
+                  <Button onClick={handleCookieAccept}>Accept Cookies</Button>
+                  <Button variant="ghost" onClick={handleCookieDecline}>
+                    Decline Cookies
+                  </Button>
+                </ButtonGroup>
+              </motion.div>
+            </Box>
+          ),
+          end: true,
+          hideInput: true,
+        },
+      ]}
+    />
   );
 };
 
 const Introduction = () => {
-  const [isComplete, setIsComplete] = React.useState(false);
-  const init = (typewriter) => {
-    typewriter
-      .pauseFor(1000)
-      .typeString("What would you like to do?")
-      .start()
-      .callFunction(() => setIsComplete(!isComplete));
-  };
   return (
-    <>
-      <chakra.div
-        color="font.100"
-        fontSize={{
-          base: "4xl",
-          sm: "5xl",
-          md: "6xl",
-          xl: "8xl",
-        }}
-      >
-        <Typewriter
-          onInit={init}
-          options={{
-            loop: false,
-            delay: 16,
-          }}
-        />
-      </chakra.div>
-      {isComplete ? (
-        <Box>
-          {console.log("completed")}
-          <motion.div
-            initial={{ opacity: 0, y: "0px" }}
-            whileInView={{ opacity: 1, y: "-5px" }}
-            transition={{ duration: 0.4, delay: 0.2 }}
-            viewport={{ once: true }}
-          >
-            <ButtonGroup pt={16} spacing={6} colorScheme="purple" size="lg">
-              <Button>Read Guide</Button>
-              <Button variant="link">Learn more about</Button>
-            </ButtonGroup>
-          </motion.div>
-        </Box>
-      ) : (
-        console.log("not completed")
-      )}
-    </>
+    <ChatBot
+      hideHeader={true}
+      width="100%"
+      height="auto"
+      hideBotAvatar={true}
+      hideUserAvatar={true}
+      className="default"
+      steps={[
+        {
+          id: "1",
+          message: "Hi, what do you want do to?",
+          trigger: "2",
+          hideInput: true,
+        },
+        {
+          id: "2",
+          component: (
+            <Box>
+              {console.log("completed")}
+              <motion.div
+                initial={{ opacity: 0, y: "0px" }}
+                whileInView={{ opacity: 1, y: "-5px" }}
+                transition={{ duration: 0.4, delay: 0.2 }}
+                viewport={{ once: true }}
+              >
+                <ButtonGroup pt={16} colorScheme="purple" size="lg">
+                  <Button as={GatsbyLink} to="/docs/">
+                    Read Guide
+                  </Button>
+                  <Button variant="ghost">Learn more about</Button>
+                </ButtonGroup>
+              </motion.div>
+            </Box>
+          ),
+          end: true,
+          hideInput: true,
+        },
+      ]}
+    />
   );
 };
 
 const CookieConsent = ({ cookies, setCookie, removeCookie }) => {
-  console.log(cookies.consent);
-  if (cookies.consent === "accepted") return <Introduction />;
-  else return <AskForCookie {...{ setCookie, removeCookie }} />;
+  console.log(cookies._ga);
+  if (cookies.consent === undefined)
+    return <AskForCookie {...{ setCookie, removeCookie }} />;
+  else return <Introduction />;
 };
 
 export default CookieConsent;
