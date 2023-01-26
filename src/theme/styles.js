af3dbe069a664f3906acbc383c0bc7a6b72116b6
Lars Mäder <mail@larsmaeder.ch>
2023-01-26 16:54:11 +0100

    style: hide overflow scrollbar

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -24,17 +24,13 @@ export const GlobalStyles = {
     ".default .rsc-content": {
       p: 0,
       m: 0,
+      overflowY: "inherit",
     },
     ".default .rsc-ts-image-container": {
       display: "none",
     },
     ".default .rsc-ts-bubble": {
       fontSize: { base: "md", sm: "lg", md: "xl", xl: "2xl" },
-      // borderTopLeftRadius: 12,
-      // borderTopRightRadius: 12,
-      // borderBottomRightRadius: 12,
-      // borderBottomLeftRadius: 0,
-      // borderRadius: "12px 4 4 0",
       color: "inherit",
       py: { base: 3, md: 4 },
       px: { base: 4, md: 6 },
