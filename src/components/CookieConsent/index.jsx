d77d6f08816680b014de66cdbf43473d5524deea
Lars Mäder <mail@larsmaeder.ch>
2023-01-27 16:08:41 +0100

    refactor: wording

--- src/components/CookieConsent/index.jsx
+++ src/components/CookieConsent/index.jsx
@@ -1,7 +1,14 @@
 import * as React from "react";
 import { Link as GatsbyLink } from "gatsby";
 import { motion } from "framer-motion";
-import { Button, ButtonGroup, Box, Text } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  Box,
+  Text,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
 import ChatBot from "react-simple-chatbot";
 
 const AskForCookie = ({ setCookie, removeCookie }) => {
@@ -11,7 +18,7 @@ const AskForCookie = ({ setCookie, removeCookie }) => {
   };
   const handleCookieDecline = () => {
     setCookie("consent", "declined", { path: "/" });
-    removeCookie("_ga", { path: "/" });
+    // removeCookie("_ga", { path: "/" });
     console.log("Declined");
   };
   return (
@@ -91,12 +98,55 @@ const Introduction = () => {
       steps={[
         {
           id: "1",
-          message: "Hi, what do you want do to?",
+          message: "Welcome to VUI Guide.",
           trigger: "2",
           hideInput: true,
         },
         {
           id: "2",
+          message: "You have several options to explore:",
+          trigger: "3",
+          hideInput: true,
+        },
+        {
+          id: "3",
+          trigger: "4",
+          component: (
+            <Box>
+              <motion.div
+                initial={{ opacity: 0, y: "0px" }}
+                whileInView={{ opacity: 1, y: "-5px" }}
+                transition={{ duration: 0.4, delay: 0.2 }}
+                viewport={{ once: true }}
+              >
+                <UnorderedList fontSize="2xl" pl={6} pt={4}>
+                  <ListItem>
+                    You can read through the guide to understand
+                  </ListItem>
+                  <ListItem>
+                    You can also learn more about the project and how you can
+                    help and make meaningful changes to the platform.
+                  </ListItem>
+                </UnorderedList>
+              </motion.div>
+            </Box>
+          ),
+          hideInput: true,
+        },
+        // {
+        //   id: "3",
+        //   message: "You can read through the guide to understand ",
+        //   trigger: "4",
+        //   hideInput: true,
+        // },
+        // {
+        //   id: "4",
+        //   message: "You can also learn more about the project and how you can help and make meaningful changes to the platform.",
+        //   trigger: "5",
+        //   hideInput: true,
+        // },
+        {
+          id: "4",
           component: (
             <Box>
               {console.log("completed")}
@@ -108,9 +158,9 @@ const Introduction = () => {
               >
                 <ButtonGroup pt={16} colorScheme="purple" size="lg">
                   <Button as={GatsbyLink} to="/docs/">
-                    Read Guide
+                    Read the Guide
                   </Button>
-                  <Button variant="ghost">Learn more about</Button>
+                  <Button variant="ghost">Learn more about it</Button>
                 </ButtonGroup>
               </motion.div>
             </Box>
@@ -124,7 +174,7 @@ const Introduction = () => {
 };
 
 const CookieConsent = ({ cookies, setCookie, removeCookie }) => {
-  setCookie("_ga", "FSJBFRFJB", { path: "/" });
+  // setCookie("_ga", "FSJBFRFJB", { path: "/" });
   console.log(cookies._ga);
   if (cookies.consent === undefined)
     return <AskForCookie {...{ setCookie, removeCookie }} />;
