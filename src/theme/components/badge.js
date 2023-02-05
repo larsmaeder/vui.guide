a4c6a57b6bfeef44e12d88fd4e572311c50676e9
larsmaeder <mail@larsmaeder.ch>
2023-02-05 01:02:44 +0100

    style: badge

---
+++ src/theme/components/badge.js
@@ -0,0 +1,14 @@
+import { defineStyleConfig } from "@chakra-ui/react";
+
+export const BadgeStyles = defineStyleConfig({
+  // Styles for the base style
+  baseStyle: {
+    fontFamily: "heading",
+  },
+  // Styles for the size variations
+  sizes: {},
+  // Styles for the visual style variations
+  variants: {},
+  // The default `size` or `variant` values
+  defaultProps: {},
+});
