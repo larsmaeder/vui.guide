27d9ae41937e5898fd69b38c5a24b531a269e55d
Lars Mäder <mail@larsmaeder.ch>
2023-02-22 17:11:47 +0100

    refactor: remove sass

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,8 @@
 import * as React from "react";
-import * as Style from "./404.module.scss";
 
 const PageNotFound = () => (
   <>
-    <div className={Style.notFound}>
+    <div>
       <h1>404: Page not found</h1>
       <p>You just hit a route that doesn’t exist... the sadness :(</p>
     </div>
