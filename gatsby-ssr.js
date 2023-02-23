4a9ed36ef812714c3404d7ad3d99883d3234674f
Lars Mäder <mail@larsmaeder.ch>
2023-02-23 17:11:00 +0100

    chore(build): add browser copy as ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,6 @@
+import "@fontsource/readex-pro/400.css"; // Normal
+import "@fontsource/readex-pro/700.css"; // Bold
+import "@fontsource/kaisei-decol/400.css"; // Normal
+import "@fontsource/kaisei-decol/700.css"; // Bold
+import "@fontsource/roboto-mono/400.css"; // Normal
+import "@fontsource/roboto-mono/700.css"; // Bold
