6364a12f86d9595c6314e6bbeedd75f1a17fb34d
larsmaeder <mail@larsmaeder.ch>
2023-02-26 20:22:14 +0100

    chore(build): url

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 const {
   NODE_ENV,
-  URL: NETLIFY_SITE_URL = "https://www.vui.guide",
+  URL: NETLIFY_SITE_URL = "https://beta.vui.guide",
   DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
   CONTEXT: NETLIFY_ENV = NODE_ENV,
 } = process.env;
